307f6f1acf5a646333130714c92662e7c0f5dc60
misla <misla@misla.misla>
2020-05-09 13:06:23 +0200

    Added intent width to clang format options. Changed makefile to perform a clean instead of a test (no rules for test rigth now). Exectuted format

--- include/linux_parser.h
+++ include/linux_parser.h
@@ -29,16 +29,16 @@ std::string Kernel();
 
 // CPU
 enum CPUStates {
-  kUser_ = 0,
-  kNice_,
-  kSystem_,
-  kIdle_,
-  kIOwait_,
-  kIRQ_,
-  kSoftIRQ_,
-  kSteal_,
-  kGuest_,
-  kGuestNice_
+    kUser_ = 0,
+    kNice_,
+    kSystem_,
+    kIdle_,
+    kIOwait_,
+    kIRQ_,
+    kSoftIRQ_,
+    kSteal_,
+    kGuest_,
+    kGuestNice_
 };
 std::vector<std::string> CpuUtilization();
 long Jiffies();
--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -12,58 +12,58 @@ using std::vector;
 
 // DONE: An example of how to read data from the filesystem
 string LinuxParser::OperatingSystem() {
-  string line;
-  string key;
-  string value;
-  std::ifstream filestream(kOSPath);
-  if (filestream.is_open()) {
-    while (std::getline(filestream, line)) {
-      std::replace(line.begin(), line.end(), ' ', '_');
-      std::replace(line.begin(), line.end(), '=', ' ');
-      std::replace(line.begin(), line.end(), '"', ' ');
-      std::istringstream linestream(line);
-      while (linestream >> key >> value) {
-        if (key == "PRETTY_NAME") {
-          std::replace(value.begin(), value.end(), '_', ' ');
-          return value;
+    string line;
+    string key;
+    string value;
+    std::ifstream filestream(kOSPath);
+    if (filestream.is_open()) {
+        while (std::getline(filestream, line)) {
+            std::replace(line.begin(), line.end(), ' ', '_');
+            std::replace(line.begin(), line.end(), '=', ' ');
+            std::replace(line.begin(), line.end(), '"', ' ');
+            std::istringstream linestream(line);
+            while (linestream >> key >> value) {
+                if (key == "PRETTY_NAME") {
+                    std::replace(value.begin(), value.end(), '_', ' ');
+                    return value;
+                }
+            }
         }
-      }
     }
-  }
-  return value;
+    return value;
 }
 
 // DONE: An example of how to read data from the filesystem
 string LinuxParser::Kernel() {
-  string os, kernel;
-  string line;
-  std::ifstream stream(kProcDirectory + kVersionFilename);
-  if (stream.is_open()) {
-    std::getline(stream, line);
-    std::istringstream linestream(line);
-    linestream >> os >> kernel;
-  }
-  return kernel;
+    string os, kernel;
+    string line;
+    std::ifstream stream(kProcDirectory + kVersionFilename);
+    if (stream.is_open()) {
+        std::getline(stream, line);
+        std::istringstream linestream(line);
+        linestream >> os >> kernel;
+    }
+    return kernel;
 }
 
 // BONUS: Update this to use std::filesystem
 vector<int> LinuxParser::Pids() {
-  vector<int> pids;
-  DIR* directory = opendir(kProcDirectory.c_str());
-  struct dirent* file;
-  while ((file = readdir(directory)) != nullptr) {
-    // Is this a directory?
-    if (file->d_type == DT_DIR) {
-      // Is every character of the name a digit?
-      string filename(file->d_name);
-      if (std::all_of(filename.begin(), filename.end(), isdigit)) {
-        int pid = stoi(filename);
-        pids.push_back(pid);
-      }
+    vector<int> pids;
+    DIR* directory = opendir(kProcDirectory.c_str());
+    struct dirent* file;
+    while ((file = readdir(directory)) != nullptr) {
+        // Is this a directory?
+        if (file->d_type == DT_DIR) {
+            // Is every character of the name a digit?
+            string filename(file->d_name);
+            if (std::all_of(filename.begin(), filename.end(), isdigit)) {
+                int pid = stoi(filename);
+                pids.push_back(pid);
+            }
+        }
     }
-  }
-  closedir(directory);
-  return pids;
+    closedir(directory);
+    return pids;
 }
 
 // TODO: Read and return the system memory utilization
