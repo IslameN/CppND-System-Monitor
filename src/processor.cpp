a5f64b3095a7c405455a396cc2b8d4cfd7f54c9b
misla <misla@misla.misla>
2020-06-10 18:20:56 +0200

    Solved reviewer commets.
    1. Changed c-style casts to c++ casts (static_cast).
    1. Calculated properly the Uptime of a process, considering the system uptime.
    1. Removed unused functions.
    1. Added const to const variables.
    
    Added this features.
    1. Removed using directives.

--- src/processor.cpp
+++ src/processor.cpp
@@ -7,26 +7,26 @@
 float Processor::Utilization() {
     std::vector<std::string> cpu_values = LinuxParser::CpuUtilization();
 
-    int current_user = atoi(cpu_values[0].c_str());
-    int current_nice = atoi(cpu_values[1].c_str());
-    int current_system = atoi(cpu_values[2].c_str());
-    int current_idle = atoi(cpu_values[3].c_str());
-    int current_iowait = atoi(cpu_values[4].c_str());
-    int current_irq = atoi(cpu_values[5].c_str());
-    int current_softirq = atoi(cpu_values[6].c_str());
-    int current_steal = atoi(cpu_values[7].c_str());
-    int current_guest = atoi(cpu_values[8].c_str());
-    int current_guest_nice = atoi(cpu_values[9].c_str());
+    const int current_user = atoi(cpu_values[0].c_str());
+    const int current_nice = atoi(cpu_values[1].c_str());
+    const int current_system = atoi(cpu_values[2].c_str());
+    const int current_idle = atoi(cpu_values[3].c_str());
+    const int current_iowait = atoi(cpu_values[4].c_str());
+    const int current_irq = atoi(cpu_values[5].c_str());
+    const int current_softirq = atoi(cpu_values[6].c_str());
+    const int current_steal = atoi(cpu_values[7].c_str());
+    const int current_guest = atoi(cpu_values[8].c_str());
+    const int current_guest_nice = atoi(cpu_values[9].c_str());
 
-    int prev_idle = idle + iowait;
-    int new_idle = current_idle + current_iowait;
-    int prev_non_idle = user + nice + system + irq + softirq + steal;
-    int non_idle = current_user + current_nice + current_system + current_irq +
+    const int prev_idle = idle + iowait;
+    const int new_idle = current_idle + current_iowait;
+    const int prev_non_idle = user + nice + system + irq + softirq + steal;
+    const int non_idle = current_user + current_nice + current_system + current_irq +
                    current_softirq + current_steal;
-    int prev_total = prev_idle + prev_non_idle;
-    int total = new_idle + non_idle;
-    int total_diff = total - prev_total;
-    int idle_diff = new_idle - prev_idle;
+    const int prev_total = prev_idle + prev_non_idle;
+    const int total = new_idle + non_idle;
+    const int total_diff = total - prev_total;
+    const int idle_diff = new_idle - prev_idle;
 
     user = current_user;
     nice = current_nice;
@@ -39,5 +39,5 @@ float Processor::Utilization() {
     guest = current_guest;
     guest_nice = current_guest_nice;
 
-    return (float)(total_diff - idle_diff) / total_diff;
+    return static_cast<float>(total_diff - idle_diff) / total_diff;
 }
